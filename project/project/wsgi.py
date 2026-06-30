82550152342e13b14e9a3c072a36b04549bae001
tyransoft <tyransoftagency@gmail.com>
2026-06-30 14:37:30 +0200

    8

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -1,6 +1,11 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
+
+BASE_DIR = "/var/www/webroot/ROOT/project"
+
+sys.path.insert(0, BASE_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
