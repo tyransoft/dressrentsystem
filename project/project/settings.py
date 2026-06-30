c2b716de81770bb3e07d2b61f1e87b976dbab7fd
tyransoft <tyransoftagency@gmail.com>
2026-06-30 17:31:52 +0200

    3422

--- project/project/settings.py
+++ project/project/settings.py
@@ -121,15 +121,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/var/www/webroot/ROOT/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
