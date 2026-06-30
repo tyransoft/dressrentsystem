629ded08297ab4b5e69cbdb22393ee3d44ca4cb2
tyransoft <tyransoftagency@gmail.com>
2026-06-30 14:13:00 +0200

    5

--- project/project/settings.py
+++ project/project/settings.py
@@ -75,14 +75,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-"""
+# https://docs.djangoproject.com/en/6.0/ref/settings/#database
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -93,7 +87,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
