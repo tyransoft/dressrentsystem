b6782b48a5f05788239857b2a1f7cce35fc73876
tyransoft <tyransoftagency@gmail.com>
2026-06-30 14:32:20 +0200

    7

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -1,8 +1,4 @@
 import os
-import sys
-
-sys.path.insert(0, "/var/www/webroot/ROOT/project")
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
