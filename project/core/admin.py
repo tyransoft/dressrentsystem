c1715eb600ac3567a5c6fecbd0208f11c2103590
tyransoft <tyransoftagency@gmail.com>
2026-08-15 12:20:21 +0200

    cx

--- project/core/admin.py
+++ project/core/admin.py
@@ -3,4 +3,4 @@ from .models import *
 # Register your models here.
 admin.site.register(CustomUser)
 admin.site.register(Invoice)
-admin(MonthlySalaryPayment)
+admin.site.register(MonthlySalaryPayment)
