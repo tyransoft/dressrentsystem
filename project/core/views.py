1149b4c273398b060ffb5f354814a401ac488475
tyransoft <tyransoftagency@gmail.com>
2026-09-19 12:07:24 +0200

    675

--- project/core/views.py
+++ project/core/views.py
@@ -1100,7 +1100,7 @@ def create_invoice(request):
                 Q(barcode__icontains=search)
             )
 
-        paginator = Paginator(products, 15)
+        paginator = Paginator(products, 50)
         page_number = request.GET.get('page', 1)
         products_page = paginator.get_page(page_number)
 
